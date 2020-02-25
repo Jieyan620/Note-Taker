6babf73c6311f1fd65045fc1e385f605aae01cc3
Jie Yan <jieyan620@gmail.com>
2020-02-25 00:10:58 -0800

    add files

---
+++ Develop/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, note) => {
+    if (err) { console.log(err) }
+    res.json(JSON.parse(note))
+  })
+})
+
+app.get('/notes/:item', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    const note = JSON.parse(data)
+    res.json(note.filter(text => note.title === req.params.item)[0])
+  })
+})
+
+app.post('/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    const note = JSON.parse(data)
+    note.push(req.body)
+    fs.writeFile('./db/db.json', JSON.stringify(note), err => {
+      if (err) { console.log(err) }
+      res.sendStatus(200)
+    })
+  })
+})
+
+app.listen(3000)
